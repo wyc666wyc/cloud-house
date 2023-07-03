19eb12cd10d86b9c3735cb02f5841abe9f020788
wuyuc <wuyuc@shencai-china.com>
2023-07-03 21:22:09 +0800

    update: echarts编辑器开发

--- packages/echarts-editor/src/config/optionConfig.ts
+++ packages/echarts-editor/src/config/optionConfig.ts
@@ -1,6 +1,20 @@
-import { TitleComponentOption } from 'echarts'
+import {
+  TitleComponentOption,
+  XAXisComponentOption
+} from 'echarts'
 
 export const title: TitleComponentOption = {
   id: 0,
+  show: false,
   textStyle: {}
+}
+
+export const xAxis: XAXisComponentOption = {
+  id: 0,
+  show: false,
+  axisLabel: {},
+  axisLine: {},
+  axisTick: {},
+  splitLine: {},
+
 }

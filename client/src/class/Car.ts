e7a7109991ee94a3ed0077bfd6a0560ce2b2eabd
www <824189047@qq.com>
2023-10-15 22:46:52 +0800

    add damage

--- client/src/class/Car.ts
+++ client/src/class/Car.ts
@@ -14,6 +14,7 @@ export default class Car {
   angleScale: number
   controls: Controls
   sensor: Sensor
+  polygon: Coordinate[]
   constructor(x: number, y: number, w: number, h: number) {
     this.x = x
     this.y = y
@@ -25,12 +26,36 @@ export default class Car {
     this.friction = 0.02
     this.angle = 0
     this.angleScale = 0.03
+    this.polygon = []
     this.controls = new Controls()
     this.sensor = new Sensor(this)
   }
   update(roadBorders: Coordinate[][]) {
     this.#move()
     this.sensor.update(roadBorders)
+    this.polygon = this.#createPolygon()
+  }
+  #createPolygon() {
+    const points = []
+    const rad = Math.hypot(this.w, this.h) / 2
+    const alpha = Math.atan2(this.w, this.h)
+    points.push({
+      x: this.x - Math.sin(this.angle - alpha) * rad,
+      y: this.y - Math.cos(this.angle - alpha) * rad
+    })
+    points.push({
+      x: this.x - Math.sin(this.angle + alpha) * rad,
+      y: this.y - Math.cos(this.angle + alpha) * rad
+    })
+    points.push({
+      x: this.x - Math.sin(Math.PI + this.angle - alpha) * rad,
+      y: this.y - Math.cos(Math.PI + this.angle - alpha) * rad
+    })
+    points.push({
+      x: this.x - Math.sin(Math.PI + this.angle + alpha) * rad,
+      y: this.y - Math.cos(Math.PI + this.angle + alpha) * rad
+    })
+    return points
   }
   #move() {
     if (this.controls.forward) {
@@ -68,18 +93,25 @@ export default class Car {
     this.y -= this.speed
   }
   draw(ctx: CanvasRenderingContext2D) {
-    ctx.save()
-    ctx.translate(this.x, this.y)
-    ctx.rotate(-this.angle)
     ctx.beginPath()
-    ctx.rect(
-      - this.w / 2,
-      - this.h / 2,
-      this.w,
-      this.h
-    )
+    ctx.moveTo(this.polygon[0].x, this.polygon[0].y)
+    for(let i = 1; i < this.polygon.length; i++) {
+      const { x, y } = this.polygon[i]
+      ctx.lineTo(x, y)
+    }
     ctx.fill()
-    ctx.restore()
+    // ctx.save()
+    // ctx.translate(this.x, this.y)
+    // ctx.rotate(-this.angle)
+    // ctx.beginPath()
+    // ctx.rect(
+    //   - this.w / 2,
+    //   - this.h / 2,
+    //   this.w,
+    //   this.h
+    // )
+    // ctx.fill()
+    // ctx.restore()
     this.sensor.draw(ctx)
   }
 }
--- client/src/class/Sensor.ts
+++ client/src/class/Sensor.ts
@@ -1,6 +1,6 @@
 import Car from "./Car"
-import { lerp, getIntersection } from '../utils'
-import { Coordinate, Reading } from '../../types'
+import { lerp, getIntersection } from "../utils"
+import { Coordinate, Reading } from "../../types"
 
 export default class Sensor {
   car: Car
@@ -8,7 +8,7 @@ export default class Sensor {
   rayLength: number
   raySpred: number
   rays: Coordinate[][]
-  readings: (Reading | null | undefined) []
+  readings: (Reading | null | undefined)[]
   constructor(car: Car, rayCount = 10) {
     this.car = car
     this.rayCount = rayCount
@@ -20,44 +20,48 @@ export default class Sensor {
   update(roadBorders: Coordinate[][]) {
     this.#castRays()
     this.readings = []
-    this.rays.forEach(ray => {
-      this.readings.push(
-        this.#getReadings(ray, roadBorders)
-      )
+    this.rays.forEach((ray) => {
+      this.readings.push(this.#getReadings(ray, roadBorders))
     })
   }
   #getReadings(ray: Coordinate[], roadBorders: Coordinate[][]) {
-    const touches: Reading [] = []
-    roadBorders.forEach(border => {
+    const touches: Reading[] = []
+    roadBorders.forEach((border) => {
       const touch = getIntersection(ray[0], ray[1], border[0], border[1])
       touch && touches.push(touch)
     })
     if (touches.length === 0) {
       return null
     }
-    const offset = touches.map(e => e.offset)
+    const offset = touches.map((e) => e.offset)
     const minOffset = Math.min(...offset)
-    return touches.find(e => e.offset === minOffset) 
+    return touches.find((e) => e.offset === minOffset)
   }
   #castRays() {
     this.rays.length = 0
     const { x, y } = this.car
-    for(let i = 0; i < this.rayCount; i++) {
-      const angle = lerp(this.raySpred / 2, -this.raySpred / 2, this.rayCount === 1 ? 0.5 : (i / (this.rayCount - 1))) + this.car.angle
+    for (let i = 0; i < this.rayCount; i++) {
+      const angle =
+        lerp(
+          this.raySpred / 2,
+          -this.raySpred / 2,
+          this.rayCount === 1 ? 0.5 : i / (this.rayCount - 1)
+        ) + this.car.angle
       const start = { x, y }
       const end = {
         x: x - Math.sin(angle) * this.rayLength,
-        y: y - Math.cos(angle) * this.rayLength
+        y: y - Math.cos(angle) * this.rayLength,
       }
       this.rays.push([start, end])
     }
   }
   draw(ctx: CanvasRenderingContext2D) {
-    this.rays.forEach(([start, end]) => {
+    this.rays.forEach(([start, end], i) => {
+      this.readings[i] && (end = this.readings[i] as Coordinate)
       ctx.beginPath()
-      ctx.strokeStyle = 'yellow'
+      ctx.strokeStyle = "yellow"
       ctx.lineWidth = 2
-  
+
       ctx.moveTo(start.x, start.y)
       ctx.lineTo(end.x, end.y)
       ctx.stroke()
